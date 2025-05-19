52df3bfc96aeef4441db1f23cf05354731a48994
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2025-05-19 10:29:43 +0200

    leila gud

--- student.py
+++ student.py
@@ -13,11 +13,13 @@ class Student:
         self.score += points 
 
 
-tivadar = Student("El Tivadar", 16, "male")
+tivadar = Student("El Tivadar","male" , 16)
 leila = Student("Los Leila", "female")
 
 tivadar.introduce()
 tivadar.learn(12)
 tivadar.introduce()
 
+leila.introduce()
+leila.learn(-2)
 leila.introduce()
