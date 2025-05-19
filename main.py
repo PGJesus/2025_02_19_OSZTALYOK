58701fb9ec8d6ce5803da6d9a12dcc5be1f685a6
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2025-05-19 10:41:09 +0200

    with imprt

---
+++ main.py
@@ -0,0 +1,12 @@
+from student import Student
+
+tivadar = Student("El Tivadar","male" , 16)
+leila = Student("Los Leila", "female")
+
+tivadar.introduce()
+tivadar.learn(12)
+tivadar.introduce()
+
+leila.introduce()
+leila.learn(-2)
+leila.introduce()
